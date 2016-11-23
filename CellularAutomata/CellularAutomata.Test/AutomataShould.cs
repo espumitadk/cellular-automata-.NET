d70c8fc2340c89fc6cb1d87e6716d4cb0e229d0b
David Falcón Naranjo <david_el_125@hotmail.com>
2016-11-23 23:49:26 +0100

    Refactor: extract given method

--- CellularAutomata/CellularAutomata.Test/AutomataShould.cs
+++ CellularAutomata/CellularAutomata.Test/AutomataShould.cs
@@ -11,15 +11,19 @@ namespace CellularAutomata.Test {
         [Test]
         public void apply_given_rule_to_each_cell() {
             var rule30 =  Substitute.For<Rule30>();
-            List<Cell> givenCells = new List<Cell>{
-                Cell.Alive, Cell.Alive, Cell.Death, Cell.Alive, Cell.Death, Cell.Alive, Cell.Death
-            };
-            var automata = new Automata(givenCells, rule30);
+            var automata = new Automata(GivenCells(), rule30);
 
             automata.Iterate();
 
             rule30.Received(7).Apply(Arg.Any<Neighborhood>());
         }
+
+        private static List<Cell> GivenCells() {
+            return new List<Cell>{
+                Cell.Alive, Cell.Alive, Cell.Death, Cell.Alive, Cell.Death, Cell.Alive, Cell.Death
+            };
+        }
+
     }
 
 }
