13caf90c6fae9d1a6e221f43a51500c5d83fbffe
David Falcón Naranjo <david_el_125@hotmail.com>
2016-11-23 22:20:51 +0100

    setup C# environment

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject cellular-automata "0.0.1-SNAPSHOT"
-  :description "Cellular automata kata"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}
-             :midje {}})
