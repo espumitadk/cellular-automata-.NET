dbab6810937204c95ea8893d95fc298de4d4bb80
David Falcón Naranjo <david_el_125@hotmail.com>
2016-11-28 10:03:17 +0100

    green: give_a_death_cell_when_neighborhood_is_

--- CellularAutomata/CellularAutomata/Neighborhood.cs
+++ CellularAutomata/CellularAutomata/Neighborhood.cs
@@ -2,8 +2,35 @@ namespace CellularAutomata {
 
     public class Neighborhood {
 
-        public Neighborhood(Cell leftCell, Cell centerCell, Cell rightCell) {
+        private readonly Cell leftCell;
+        private readonly Cell centerCell;
+        private readonly Cell rightCell;
 
+        public Neighborhood(Cell leftCell, Cell centerCell, Cell rightCell) {
+            this.leftCell = leftCell;
+            this.centerCell = centerCell;
+            this.rightCell = rightCell;
+        }
+
+        protected bool Equals(Neighborhood other) {
+            return leftCell == other.leftCell && centerCell == other.centerCell && rightCell == other.rightCell;
+        }
+
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Neighborhood) obj);
+        }
+
+        public override int GetHashCode() {
+            unchecked
+            {
+                var hashCode = (int) leftCell;
+                hashCode = (hashCode*397) ^ (int) centerCell;
+                hashCode = (hashCode*397) ^ (int) rightCell;
+                return hashCode;
+            }
         }
 
     }
--- CellularAutomata/CellularAutomata/Rule30.cs
+++ CellularAutomata/CellularAutomata/Rule30.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
+
 namespace CellularAutomata {
 
     public class Rule30 {
+        
+        private readonly Dictionary<Neighborhood, Cell> equivalences = new Dictionary<Neighborhood, Cell> {
+            { new Neighborhood(Cell.Alive, Cell.Alive, Cell.Alive), Cell.Death },
+            { new Neighborhood(Cell.Alive, Cell.Alive, Cell.Death), Cell.Death },
+            { new Neighborhood(Cell.Alive, Cell.Death, Cell.Alive), Cell.Death },
+            { new Neighborhood(Cell.Death, Cell.Death, Cell.Death), Cell.Death }
+        };
 
-        public virtual void Apply(Neighborhood neighborhood) {
-            
+        public virtual Cell Apply(Neighborhood neighborhood) {
+            return equivalences[neighborhood];
         }
 
     }
