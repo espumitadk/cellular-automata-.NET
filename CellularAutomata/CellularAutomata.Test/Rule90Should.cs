3e65c10762166d09c1d3781094332dcf3125c1ae
David Falcón Naranjo <david_el_125@hotmail.com>
2016-11-29 11:44:16 +0100

    Green: rule90 logic tests

---
+++ CellularAutomata/CellularAutomata.Test/Rule90Should.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CellularAutomata.Test {
+
+    [TestFixture]
+    public class Rule90Should
+    {
+
+        [TestCase(Cell.Alive, Cell.Alive, Cell.Alive)]
+        [TestCase(Cell.Alive, Cell.Death, Cell.Alive)]
+        [TestCase(Cell.Death, Cell.Alive, Cell.Death)]
+        [TestCase(Cell.Death, Cell.Death, Cell.Death)]
+        public void give_a_death_cell_when_neighborhood_is_(Cell leftCell, Cell centerCell, Cell rightCell)
+        {
+            var rule = new Rule90();
+            var neighborhood = new Neighborhood(leftCell, centerCell, rightCell);
+
+            var evolvedCell = rule.Apply(neighborhood);
+
+            evolvedCell.Should().Be(Cell.Death);
+        }
+
+        [TestCase(Cell.Alive, Cell.Alive, Cell.Death)]
+        [TestCase(Cell.Alive, Cell.Death, Cell.Death)]
+        [TestCase(Cell.Death, Cell.Alive, Cell.Alive)]
+        [TestCase(Cell.Death, Cell.Death, Cell.Alive)]
+        public void give_a_alive_cell_when_neighborhood_is_(Cell leftCell, Cell centerCell, Cell rightCell)
+        {
+            var rule = new Rule90();
+            var neighborhood = new Neighborhood(leftCell, centerCell, rightCell);
+
+            var evolvedCell = rule.Apply(neighborhood);
+
+            evolvedCell.Should().Be(Cell.Alive);
+        }
+
+    }
+
+}
---
+++ CellularAutomata/CellularAutomata/Rule90.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace CellularAutomata {
+
+    public class Rule90 {
+
+        private readonly Dictionary<Neighborhood, Cell> equivalences = new Dictionary<Neighborhood, Cell> {
+            { new Neighborhood(Cell.Alive, Cell.Alive, Cell.Alive), Cell.Death },
+            { new Neighborhood(Cell.Alive, Cell.Death, Cell.Alive), Cell.Death },
+            { new Neighborhood(Cell.Death, Cell.Alive, Cell.Death), Cell.Death },
+            { new Neighborhood(Cell.Death, Cell.Death, Cell.Death), Cell.Death }
+        };
+
+        public virtual Cell Apply(Neighborhood neighborhood)
+        {
+            return equivalences.ContainsKey(neighborhood) ? Cell.Death : Cell.Alive;
+        }
+
+    }
+
+}
